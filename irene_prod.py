907f28b959cf36eecb8c7a3fb88421cb1bf98dc4
jmbenlloch <info@jmbenlloch.net>
2017-09-20 12:14:02 +0200

    Create branch for MC

--- irene_prod.py
+++ irene_prod.py
@@ -1,226 +1,125 @@
-from __future__ import division
 from __future__ import print_function
 
-import os
 import argparse
-import magic
+from time import sleep
 import re
+from subprocess import call
+import sys, os
+from datetime import datetime
+from os.path import dirname
+from glob import glob
 
-from math       import ceil
-from time       import sleep
-from subprocess import check_output, call, CalledProcessError
-from glob       import glob
 
-def find_pattern(pattern, fname, group_name):
-    result = ''
-    data = open(fname).readlines()
-    for line in data:
-        pattern = re.compile(pattern)
-        match = pattern.match(line)
-        if match:
-            result =  match.group(group_name)
-            break
-    return result
-
-def get_parser():
+def get_parser(args=None):
     parser = argparse.ArgumentParser(description='Script to produce HDF5 files')
-    parser.add_argument('-j','--jobs',
-                        action   = 'store',
-                        help     = 'jobs',
-                        required = 'True')
-    parser.add_argument('-r','--run',
-                        action   = 'store',
-                        help     = 'run number',
-                        required = 'True')
+    parser.add_argument('-i','--inputpath',
+                        action='store',
+                        help='input path',
+                        required='True')
+    parser.add_argument('-o','--outputpath',
+                        action='store',
+                        help='output path',
+                        required='True')
     parser.add_argument('-t','--type',
-                        action   = 'store',
-                        help     = 'run type',
-                        required = 'True')
-    parser.add_argument('-m','--maxfiles',
-                        action   = 'store',
-                        help     = 'maximum number of files to be processed',
-                        default  = float("inf"))
-    parser.add_argument('-x','--do-not-submit',
-                        action   = 'store_true',
-                        help     = 'skip job submission if present')
+                        action='store',
+                        help='run type',
+                        required='True')
+    parser.add_argument('-c','--configpath',
+                        action='store',
+                        help='config path',
+                        required='True')
     return parser
 
-def checkmakedir(path):
-    if os.path.isdir(path):
-        print('hey, directory already exists!:\n' + path)
-    else:
-        os.makedirs(path)
-        print('creating directory...\n' + path)
-
-def get_index_from_file_name(name):
-    return int(name.split('.')[-3].split('_')[0])
-
-#Get CERES tag
-cmd = 'git describe --tags'
-output = check_output(cmd, shell=True, executable='/bin/bash')
-ceres_tag = output.strip().split('\n')[0]
-
-#Get IC tag
-exec_template_file = '/home/icuser/CERES/templates/irene.sh'
-pattern = '(\s*)export(\s*)ICTDIR(\s*)=(\s*)(?P<icrepo>.*)'
-icpath = find_pattern(pattern, exec_template_file, 'icrepo')
-cmd = 'cd {}; git describe --tags'.format(icpath)
-output = check_output(cmd, shell=True, executable='/bin/bash')
-ic_tag = output.strip().split('\n')[0]
 
 #get options
 args = get_parser().parse_args()
+opts = vars(args) # dict
+print(args)
 
-jobs     = int(args.jobs)
-run      = args.run
-runtype  = args.type
-nmax     = args.maxfiles
-skip_sub = args.do_not_submit
+input_path = args.inputpath
+output_path = args.outputpath
+output_path = args.outputpath
+config_path = args.configpath
+runtype = args.type
 
-#IO dirs
-PATHIN     = '/analysis/{}/hdf5/data/'   .format(run)
-PATHOUT    = '/analysis/{}/hdf5/pmaps/'  .format(run)
-CONFIGSDIR = '/analysis/{}/hdf5/configs/'.format(run)
-JOBSDIR    = '/analysis/{}/hdf5/jobs/'   .format(run)
-DSTDIR     = '/analysis/{}/hdf5/dst/'    .format(run)
+configs = config_path + '/config'
+exes = config_path + '/exec'
+jobs = config_path + '/jobs'
+logs = config_path + '/logs'
+
 
 #----------- check and make dirs
-checkmakedir(PATHIN)
-checkmakedir(PATHOUT)
-checkmakedir(CONFIGSDIR)
-checkmakedir(JOBSDIR)
-checkmakedir(DSTDIR)
+def checkmakedir( path ):
+    if os.path.isdir( path ):
+        print('hey, directory already exists!:\n' + path)
+    else:
+        os.makedirs( path )
+        print('creating directory...\n' + path)
 
-#remove old jobs
-map(os.remove, glob(JOBSDIR + '/*sh'))
+checkmakedir(configs)
+checkmakedir(exes)
+checkmakedir(jobs)
+checkmakedir(logs)
 
 #input files
-files = glob(PATHIN + '/*h5')
-files = sorted(files, key=get_index_from_file_name)
+files = glob(input_path + '/*h5')
+files = [f.split('/')[-1] for f in files]
+#files = sorted(files, key=lambda s: int(s.split('.')[-3].split('_')[0]))
+print( files)
 
 #open template
-#TODO read from params
+templates = { 'kr' : '/afs/cern.ch/user/j/jobenllo/newprod/templates/irene_kr.conf',
+              'na' : '/afs/cern.ch/user/j/jobenllo/newprod/templates/irene_na.conf',
+              'tl' : '/afs/cern.ch/user/j/jobenllo/newprod/templates/irene_tl.conf',
+              'cs' : '/afs/cern.ch/user/j/jobenllo/newprod/templates/irene_cs.conf'}
 
-templates = { 'alpha'    : '/home/icuser/CERES/templates/irene_alpha.conf'     ,
-              'alpha3200': '/home/icuser/CERES/templates/irene_alpha_3200.conf',
-              'alpha_s1' : '/home/icuser/CERES/templates/irene_alpha_s1.conf'  ,
-              'alpha800' : '/home/icuser/CERES/templates/irene_alpha_800.conf' ,
-              'na'       : '/home/icuser/CERES/templates/irene_na.conf'        ,
-              'kr3200'   : '/home/icuser/CERES/templates/irene_kr_3200.conf'   ,
-              'kr1300'   : '/home/icuser/CERES/templates/irene_kr_1300.conf'   ,
-              'co'       : '/home/icuser/CERES/templates/irene_co.conf'        ,
-              'co2ms'    : '/home/icuser/CERES/templates/irene_co_2ms.conf'    ,
-              'nas1'     : '/home/icuser/CERES/templates/irene_na_s1.conf'     ,
-              'nas1_200' : '/home/icuser/CERES/templates/irene_na_s1_200.conf' ,
-              'nas2'     : '/home/icuser/CERES/templates/irene_na_s2.conf'     ,
-              'nas21600' : '/home/icuser/CERES/templates/irene_na_s2_1600.conf',
-              'cs1300'   : '/home/icuser/CERES/templates/irene_cs_s2_1300.conf',
-              'cs2000'   : '/home/icuser/CERES/templates/irene_cs_s2_2000.conf',
-              'th2000'   : '/home/icuser/CERES/templates/irene_th_s2_2000.conf',
-              'bg2000'   : '/home/icuser/CERES/templates/irene_bg_s2_2000.conf'}
+#template_file = '/home/icuser/production/templates/irene_3645.conf'
+if runtype in templates:
+    template_file = templates[runtype]
+else:
+    template_file = runtype
 
-# Pick from dict of templates. Otherwise assume it is the path to a template.
-template_file = templates.get(runtype, runtype)
-template      = open(template_file).read()
-params        = {'pathin'  : PATHIN,
-                 'pathout' : PATHOUT,
-                 'run'     : run}
+template = open(template_file).read()
+exec_template_file = '/afs/cern.ch/user/j/jobenllo/newprod/templates/exec.sh'
+exec_template = open(exec_template_file).read()
 
-# build file list with outputs and filter it
-to_process = []
+job_template_file = '/afs/cern.ch/user/j/jobenllo/newprod/templates/job.submit'
+job_template = open(job_template_file).read()
 
-#check hdf5 file are completely written
-ftype = magic.Magic()
 for f in files:
-    #if file complete type would be: "Hierarchical Data Format (version 5) data"
-    if ftype.from_file(f) == 'data':
-        continue
+    config_filename = configs + '/' + f + '.conf'
+    exec_filename   = exes + '/' + f + '.sh'
+    job_filename    = jobs + '/' + f + '.submit'
+    output_filename = logs + '/' + f + '.out'
+    error_filename  = logs + '/' + f + '.err'
+    log_filename    = logs + '/' + f + '.log'
 
-    filename     = f.split('/')[-1]
-    new_name     = 'pmaps_' + ic_tag + '_' + ceres_tag
-    filename_out = filename.replace("dst_waves", new_name)
+#    fileout = f.split('.')[0].split('_')
+#    fileout[-2] = 'PMP'
+#    fileout = '_'.join(fileout) + '.h5'
+    fileout = re.sub('RWF', 'PMP', f)
+    print (fileout)
 
-    #if file already exists, skip
-    fout = PATHOUT + '/' + filename_out
-    if os.path.isfile(fout):
-        print("skip ", fout)
-        continue
+    params = {'filein'  : f,
+              'fileout' : fileout}
 
-    params['filein' ] = filename
-    params['fileout'] = filename_out
+    #write config
+    open(config_filename, 'w').write(template.format(**params))
 
-    config_file = CONFIGSDIR + '/' + filename + '.conf'
-    print(config_file)
-    open (config_file, 'w').write(template.format(**params))
+    params = {'filein'     : f,
+              'fileout'    : fileout,
+              'pathin'     : input_path,
+              'pathout'    : output_path,
+              'city'       : 'irene',
+              'configfile' : config_filename}
 
-    to_process.append(config_file)
-    if len(to_process) == nmax:
-        break
+    #write exec
+    open(exec_filename, 'w').write(exec_template.format(**params))
 
-#Generate exec files
-exec_template      = open(exec_template_file).read()
-exec_params        = {'jobsdir': JOBSDIR}
 
-jobfile    = file
-nfiles     = int(ceil(len(to_process) * 1.0 / jobs))
-count_jobs = 0
-for i, config in enumerate(to_process):
-    if i % nfiles == 0:
-        if i:
-            jobfile.write('\n\necho date\ndate\n')
-            jobfile.close()
-
-        jobfilename = JOBSDIR + '/irene_{}.sh'.format(count_jobs)
-        jobfile     = open(jobfilename, 'w')
-        jobfile.write(exec_template.format(**exec_params))
-        count_jobs += 1
-
-    cmd = 'python $ICDIR/cities/irene.py -c {}\n'.format(config)
-    jobfile.write(cmd)
-#sys.exit()
-
-if not jobfile.closed:
-    jobfile.close()
-
-#send jobs
-if not skip_sub:
-    for i in range(count_jobs):
-        cmd = 'qsub {}/irene_{}.sh'.format(JOBSDIR, i)
-        print(cmd)
-        call(cmd, shell=True, executable='/bin/bash')
-        sleep(0.3)
-
-#create dorothea job
-templates_dst = { 'alpha'    : '/home/icuser/CERES/templates/dorothea_alpha.conf'   ,
-                  'alpha800' : '/home/icuser/CERES/templates/dorothea_alpha.conf'   ,
-                  'alpha_s1' : '/home/icuser/CERES/templates/dorothea_alpha.conf'   ,
-                  'nas1'     : '/home/icuser/CERES/templates/dorothea_nas1.conf'    ,
-                  'nas1_200' : '/home/icuser/CERES/templates/dorothea_nas1_200.conf',
-                  'co'       : '/home/icuser/CERES/templates/dorothea_co.conf'      ,
-                  'kr3200'   : '/home/icuser/CERES/templates/dorothea_kr.conf'      ,
-                  'kr1300'   : '/home/icuser/CERES/templates/dorothea_kr.conf'      ,
-                  'nas2'     : '/home/icuser/CERES/templates/dorothea_nas2.conf'    ,
-                  'nas21600' : '/home/icuser/CERES/templates/dorothea_nas2.conf'    ,
-                  'cs1300'   : '/home/icuser/CERES/templates/dorothea_css2.conf'    ,
-                  'cs2000'   : '/home/icuser/CERES/templates/dorothea_css2.conf'    ,
-                  'th2000'   : '/home/icuser/CERES/templates/dorothea_ths2.conf'    ,
-                  'bg2000'   : '/home/icuser/CERES/templates/dorothea_bgs2.conf'    }
-
-if runtype in templates_dst:
-    template_file = templates_dst[runtype]
-
-    template    = open(template_file).read()
-    params      = {'run': run,
-                   'pathin': PATHIN.replace("data", "pmaps"),
-                   'pathout': PATHOUT.replace("pmaps", "dst")}
-    config_file = CONFIGSDIR + '/dorothea.conf'
-
-    print(config_file)
-    open (config_file, 'w').write(template.format(**params))
-
-    jobfilename = JOBSDIR + '/' + 'dorothea.sh'
-    jobfile     = open(jobfilename, 'w')
-    jobfile.write(exec_template.format(**exec_params))
-
-    cmd = 'python $ICDIR/cities/dorothea.py -c {}\n'.format(config_file)
-    jobfile.write(cmd)
+    params = {'exec'   : exec_filename,
+              'output' : output_filename,
+              'err'    : error_filename,
+              'log'    : log_filename}
+    #write job file
+    open(job_filename, 'w').write(job_template.format(**params))
